69ae13274ee5ce43417bd3a0f5ce7d9291bfc44d
bbuukk <bohdan.buchok@ukma.edu.ua>
2023-12-27 14:56:33 +0200

    Reformatting

---
+++ yaskrava/index.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import path from "path";
+
+// Get all .json files in the current directory
+const jsonFiles = fs
+  .readdirSync("./")
+  .filter((file) => path.extname(file) === ".json");
+
+let combinedArray = [];
+
+jsonFiles.forEach((file) => {
+  // Read each file and parse the JSON array
+  const jsonArray = JSON.parse(fs.readFileSync(file, "utf8"));
+  // Combine the arrays
+  combinedArray = [...combinedArray, ...jsonArray];
+});
+
+// Write the combined array to a new .json file
+fs.writeFileSync("combined.json", JSON.stringify(combinedArray, null, 2));
